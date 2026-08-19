bbb7ba23462096aa3d63705b47e5e6e775300b1a
skylax11 <devranpvp683@gmail.com>
2026-08-19 18:34:34 +0300

    drawing triangle at the center. kinda hello world :)

--- DGL/main.cpp
+++ DGL/main.cpp
@@ -2,6 +2,22 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+"}\0";
+
 int main() {
 
     glfwInit();
@@ -25,9 +41,57 @@ int main() {
 
     glViewport(0, 0, 800, 600);
 
+    float r{ 0.1f };
+    float g{ 0.6f };
+    float b{ 0.3f };
+
+    float vertices[] =
+    {
+        -0.5f,  -0.5f,   0,
+        0.5f,   -0.5f,   0,
+        0,      0.5f,    0
+    };
+
+    unsigned int VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // compile vertexes
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // compile fragments
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // patch those into one program 
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);  
+        glClearColor(r, g, b, 1.0f);  
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);        // activate shader program
+        glBindVertexArray(VAO);             
+        glDrawArrays(GL_TRIANGLES, 0, 3);   // draw!
 
         glfwSwapBuffers(window);
         glfwPollEvents();
